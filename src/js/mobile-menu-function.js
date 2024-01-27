08f1f2adac0bef3b6458aa891027040e5cb4b96c
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2024-01-27 11:18:06 +0200

    add js files

--- src/js/mobile-menu-function.js
+++ src/js/mobile-menu-function.js
@@ -1,15 +1,13 @@
-'use strict'
-
 const burgerButton = document.querySelector('.header-burger');
 const mobileMenuElement = document.querySelector('.mobile-menu-wrapper');
 const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
 
 function openMobileMenu() {
-    mobileMenuElement.classList.remove('visually-hidden');
+  mobileMenuElement.classList.remove('visually-hidden');
 }
 
 function closeMobileMenu() {
-    mobileMenuElement.classList.add('visually-hidden');
+  mobileMenuElement.classList.add('visually-hidden');
 }
 
 burgerButton.addEventListener('click', openMobileMenu);
