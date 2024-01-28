d131e3a7cc3e551fbcd16ff354cb7c4d13212179
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2024-01-28 19:51:59 +0200

    Update favorites.js

--- src/favorites.js
+++ src/favorites.js
@@ -1,17 +1,13 @@
-// Імпортуємо функцію displayQuote з іншого модуля
 import { displayQuote } from './js/quote.js';
 
-// Об'єкт конфігурації для цитат на сторінці "Улюблене"
 const pageFavorConfig = {
   quoteTextSelector: '.quoote-text', // Селектор для тексту цитати
   quoteAuthorSelector: '.quote-autor', // Селектор для автора цитати
 };
 
-// Перевіряємо, чи є на сторінці елементи для тексту та автора цитати
 if (
   document.querySelector(pageFavorConfig.quoteTextSelector) &&
   document.querySelector(pageFavorConfig.quoteAuthorSelector)
 ) {
-  // Якщо елементи є, відображаємо цитату
   displayQuote(pageFavorConfig);
 }
