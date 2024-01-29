6862de0bd767b59baaebd669ad5138e702cae65a
Pablo <st.spasam@gmail.com>
2024-01-30 00:31:52 +0200

    Add quote.js module to favorites.html

--- src/favorites.js
+++ src/favorites.js
@@ -1,14 +1,2 @@
 import './js/favorites-list.js';
 import './js/favorites-modal.js';
-
-// const pageFavorConfig = {
-//   quoteTextSelector: '.quoote-text', // Селектор для тексту цитати
-//   quoteAuthorSelector: '.quote-autor', // Селектор для автора цитати
-// };
-
-// if (
-//   document.querySelector(pageFavorConfig.quoteTextSelector) &&
-//   document.querySelector(pageFavorConfig.quoteAuthorSelector)
-// ) {
-//   displayQuote(pageFavorConfig);
-// }
