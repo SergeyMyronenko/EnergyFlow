7036afb6c2c481769f2c0be50e2478defdc31d9b
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2024-01-29 20:09:54 +0200

    change config

--- src/favorites.js
+++ src/favorites.js
@@ -1,15 +1,13 @@
-import { displayQuote } from './js/quote.js';
 import './js/favorites-list.js';
-import axios from 'axios';
 
-const pageFavorConfig = {
-  quoteTextSelector: '.quoote-text', // Селектор для тексту цитати
-  quoteAuthorSelector: '.quote-autor', // Селектор для автора цитати
-};
+// const pageFavorConfig = {
+//   quoteTextSelector: '.quoote-text', // Селектор для тексту цитати
+//   quoteAuthorSelector: '.quote-autor', // Селектор для автора цитати
+// };
 
-if (
-  document.querySelector(pageFavorConfig.quoteTextSelector) &&
-  document.querySelector(pageFavorConfig.quoteAuthorSelector)
-) {
-  displayQuote(pageFavorConfig);
-}
+// if (
+//   document.querySelector(pageFavorConfig.quoteTextSelector) &&
+//   document.querySelector(pageFavorConfig.quoteAuthorSelector)
+// ) {
+//   displayQuote(pageFavorConfig);
+// }
