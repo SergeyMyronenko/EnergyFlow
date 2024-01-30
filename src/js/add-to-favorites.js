981093adb85fc94b9dc23647c9087635829148cd
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2024-01-30 17:33:01 +0200

    Update add-to-favorites.js

--- src/js/add-to-favorites.js
+++ src/js/add-to-favorites.js
@@ -1,7 +1,8 @@
 import { getData } from './exercises-modal.js';
 const addToFavoritesBtn = document.querySelector('.modal-button-favorites');
+const modalExercises = document.querySelector('.modal');
 const modalWindow = document.querySelector('.modal');
-document.addEventListener('click', addToFavorites);
+modalExercises.addEventListener('click', addToFavorites);
 
 export const LOCAL_STORAGE_KEY = 'favoriteData';
 export const inLocalStorage =
@@ -9,7 +10,6 @@ export const inLocalStorage =
 
 async function addToFavorites(e) {
   try {
-    // const itemId = await getData(e.target.dataset.id);
     if (e.target.classList.contains('modal-button-favorites')) {
       const itemId = await getData(e.target.dataset.id);
       const data = itemId.data;
@@ -25,7 +25,5 @@ async function addToFavorites(e) {
     }
   } catch (error) {
     console.log(error.message);
-  } finally {
-    addToFavoritesBtn.removeEventListener('click', addToFavorites);
   }
 }
