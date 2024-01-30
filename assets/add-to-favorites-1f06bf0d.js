bea59c7a22540ea8eec79c6c19a51a14573df0b4
SergeyMyronenko <55333290+SergeyMyronenko@users.noreply.github.com>
2024-01-30 18:12:26 +0000

    Deploying to gh-pages from @ SergeyMyronenko/EnergyFlow@f6b44155ce5992700bdb13f6145ec91123f99a44 🚀

---
+++ assets/add-to-favorites-1f06bf0d.js
@@ -0,0 +1,85 @@
+import{a as c}from"./vendor-20b07447.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))n(o);new MutationObserver(o=>{for(const s of o)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function i(o){const s={};return o.integrity&&(s.integrity=o.integrity),o.referrerPolicy&&(s.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?s.credentials="include":o.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(o){if(o.ep)return;o.ep=!0;const s=i(o);fetch(o.href,s)}})();const g=document.querySelector(".header-burger"),d=document.querySelector(".mobile-menu-wrapper"),f=document.querySelector(".mobile-menu-close-btn"),v=document.querySelector(".mobile-menu-btn-content-home"),y=document.querySelector(".mobile-menu-btn-content-favorites");function q(){d.classList.remove("visually-hidden")}function h(){d.classList.add("visually-hidden")}g.addEventListener("click",q);f.addEventListener("click",h);v.addEventListener("click",()=>{window.location.href="./"});y.addEventListener("click",()=>{window.location.href="favorites.html"});async function L(){const e=localStorage&&JSON.parse(localStorage.getItem("quoteData")),t=new Date().getDate();return e&&e.timestamp===t?e:S()}async function S(){try{const e=await c.get("https://energyflow.b.goit.study/api/quote"),t={quote:e.data.quote,author:e.data.author,timestamp:new Date().getDate()};return localStorage.setItem("quoteData",JSON.stringify(t)),t}catch(e){console.error("Помилка:",e)}}async function x(e){try{const t=await L(),i=document.querySelector(e.quoteTextSelector),n=document.querySelector(e.quoteAuthorSelector);if(!i||!n)throw new Error("Елементи для відображення цитати не знайдено");i.innerText=t.quote,n.innerText=t.author}catch(t){console.error("Сталася помилка при відображенні цитати:",t)}}const b=[{quoteTextSelector:".quote-text",quoteAuthorSelector:".quote-author"},{quoteTextSelector:".quoote-text",quoteAuthorSelector:".quote-autor"}];b.forEach(e=>{document.querySelector(e.quoteTextSelector)&&document.querySelector(e.quoteAuthorSelector)&&x(e)});function u(e){return c.get(`https://energyflow.b.goit.study/api/exercises/${e}`)}function M(e){return`
+      <div class="modal-content">
+        <img class="modal-img" src="${e.gifUrl}" alt="">
+
+        <div class="modal-info">
+          <h4 class="modal-title">${e.name}</h4>
+
+          <div class="modal-rating">
+            <p class="modal-rating-value">${e.rating}</p>
+
+       <div class="ex-rating">
+                <div class="ex-rating-body">
+                  <div class="ex-rating-active"></div>
+                  <div class="ex-rating-items">
+                    <input
+                      type="radio"
+                      class="ex-rating-item"
+                      value="1"
+                      name="exrating"
+                    /><input
+                      type="radio"
+                      class="ex-rating-item"
+                      value="2"
+                      name="exrating"
+                    /><input
+                      type="radio"
+                      class="ex-rating-item"
+                      value="3"
+                      name="exrating"
+                    /><input
+                      type="radio"
+                      class="ex-rating-item"
+                      value="4"
+                      name="exrating"
+                    /><input type="radio" class="ex-rating-item" value="5" name="exrating" />
+                  </div>
+                </div>
+              </div>
+          </div>
+
+          <ul class="modal-list">
+            <li class="modal-list-item">
+              <p class="modal-list-item-name">Target</p>
+              <p class="modal-list-item-value">${e.target}</p>
+            </li>
+            <li class="modal-list-item">
+              <p class="modal-list-item-name">Body Part</p>
+              <p class="modal-list-item-value">${e.bodyPart}</p>
+            </li>
+            <li class="modal-list-item">
+              <p class="modal-list-item-name">Equipment</p>
+              <p class="modal-list-item-value">${e.equipment}</p>
+            </li>
+            <li class="modal-list-item">
+              <p class="modal-list-item-name">Popular</p>
+              <p class="modal-list-item-value">${e.popularity}</p>
+            </li>
+            <li class="modal-list-item">
+              <p class="modal-list-item-name">Burned calories</p>
+              <p class="modal-list-item-value">${e.burnedCalories}/3 min</p>
+            </li>
+          </ul>
+
+          <p class="modal-description">${e.description}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal-buttons">
+        <button class="modal-button-favorites" type="button" data-id="${e._id}">
+          Add to favorites
+          <svg
+            class="modal-button-favorites-icon"
+            width="20"
+            height="20"
+            aria-label="heart-icon"
+          >
+            <use href="./img/sprite.svg#heart"></use>
+          </svg>
+        </button>
+
+        <button class="modal-button-rating">Give a rating</button>
+      </div>
+  `}let E;const a={ratingModal:document.querySelector(".js-backdrop-modal"),closeBtn:document.querySelector(".js-rating-close"),rateStars:document.querySelector(".js-stars-list"),star:document.querySelectorAll(".js-rating-star"),openModalsBtn:document.querySelector(".exersizes-cards-container"),openRatingBtn:document.querySelector(".modal-button-rating"),exerciseModal:document.querySelector(".modal"),closeExerciseBtn:document.querySelector(".modal-button-close"),exsCont:document.querySelector(".exs-container")};document.addEventListener("click",w);document.addEventListener("click",T);document.addEventListener("keydown",$);function w(e){e.target.classList.contains("exersizes-card-btn")?B(e):e.target.classList.contains("modal-button-rating")&&D()}async function B(e){try{const t=await u(e.target.dataset.id);a.exsCont.innerHTML=M(t.data),a.exerciseModal.classList.add("is-open"),E=e.target.dataset.id,O()}catch(t){throw new Error(t.message)}}function D(e){a.ratingModal.classList.toggle("is-open")}function O(){const e=document.querySelector(".ex-rating-active"),t=document.querySelector(".modal-rating-value");e.style.width=`${parseInt(t.textContent)/.05}%`}function T(e){e.target.classList.contains("backdrop")&&a.ratingModal.classList.contains("is-open")?a.ratingModal.classList.remove("is-open"):e.target.classList.contains("backdrop")&&a.exerciseModal.classList.contains("is-open")||e.target.classList.contains("modal-button-close")||e.target.classList.contains("modal-button-close-icon")||e.target.classList.contains("modal-button-close-use")?a.exerciseModal.classList.remove("is-open"):(e.target.classList.contains("rating-close")||e.target.classList.contains("rating-close-svg")||e.target.classList.contains("rating-close-use"))&&a.ratingModal.classList.remove("is-open")}function $(e){e.code==="Escape"&&a.ratingModal.classList.contains("is-open")?a.ratingModal.classList.remove("is-open"):e.code==="Escape"&&a.exerciseModal.classList.contains("is-open")&&a.exerciseModal.classList.remove("is-open")}const A=document.querySelector(".modal-button-favorites"),C=document.querySelector(".modal"),k=document.querySelector(".modal");C.addEventListener("click",p);const m="favoriteData";let l=JSON.parse(localStorage.getItem(m))||[];async function p(e){try{if(e.target.classList.contains("modal-button-favorites")){const i=(await u(e.target.dataset.id)).data;if(!l.some(o=>o._id===i._id))l.push(i);else return;localStorage.setItem(m,JSON.stringify(l)),k.classList.remove("is-open")}}catch(t){console.log(t.message)}finally{A.removeEventListener("click",p)}}export{m as L,M as c,u as g,E as i};
+//# sourceMappingURL=add-to-favorites-1f06bf0d.js.map
--- commonHelpers.js
+++ commonHelpers.js
@@ -1,14 +1,15 @@
-import{g as f,c as u,L as n}from"./assets/add-to-favorites-84c8ee5e.js";import"./assets/vendor-20b07447.js";let v,r;const c=6;window.location.pathname==="/page-2.html"&&(v=document.querySelector(".favorites-list"),r=document.querySelectorAll(".favorites-pagination-block button"),l(1),r.forEach(t=>{t.addEventListener("click",()=>{const e=parseInt(t.textContent);l(e)})}));function l(t){const e=(t-1)*c,a=e+c;v.querySelectorAll(".favorites-list-item").forEach((s,i)=>{i>=e&&i<a?s.style.display="block":s.style.display="none"}),r.forEach(s=>{parseInt(s.textContent)===t?s.classList.add("active-btn"):s.classList.remove("active-btn")})}const d={exerciseModal:document.querySelector(".modal"),exsCont:document.querySelector(".exs-container")};document.addEventListener("click",g);async function g(t){try{if(t.target.classList.contains("favorites-list-button")){const e=await f("64f389465ae26083f39b18d0");d.exsCont.innerHTML=u(e.data),d.exerciseModal.classList.toggle("is-open");const a=document.querySelector(".ex-rating-active"),o=document.querySelector(".modal-rating-value");a.style.width=await`${parseInt(o.textContent)/.05}%`}}catch(e){throw new Error(e.message)}}function p(t){const e=JSON.parse(localStorage.getItem(n)),a=JSON.stringify(e.filter(o=>o._id!==t));localStorage.setItem(n,a)}function m(t){document.querySelector(`[data-id="${t}"]`).parentElement.parentElement.parentElement}const h=document.querySelector(".favorites-contanier-block");h.addEventListener("click",t=>{if(t.target.dataset.id){const e=t.target.dataset.id;p(t.target.dataset.id),m(e)}});function y(){const t=JSON.parse(localStorage.getItem("favoriteData"))||[],e=document.querySelector(".favorites-contanier-block");e.innerHTML="",t.length===0&&(e.innerHTML=`<img class="message-info-svg" src="./img/dumbbell.png" alt="dumbbell" />
+import{g as u,c as f,L as n}from"./assets/add-to-favorites-1f06bf0d.js";import"./assets/vendor-20b07447.js";let v,i;const c=6;window.location.pathname==="/page-2.html"&&(v=document.querySelector(".favorites-list"),i=document.querySelectorAll(".favorites-pagination-block button"),l(1),i.forEach(t=>{t.addEventListener("click",()=>{const e=parseInt(t.textContent);l(e)})}));function l(t){const e=(t-1)*c,r=e+c;v.querySelectorAll(".favorites-list-item").forEach((a,o)=>{o>=e&&o<r?a.style.display="block":a.style.display="none"}),i.forEach(a=>{parseInt(a.textContent)===t?a.classList.add("active-btn"):a.classList.remove("active-btn")})}const d={exerciseModal:document.querySelector(".modal"),exsCont:document.querySelector(".exs-container")};document.addEventListener("click",g);async function g(t){try{if(t.target.classList.contains("favorites-list-button")){const e=await u("64f389465ae26083f39b18d0");d.exsCont.innerHTML=f(e.data),d.exerciseModal.classList.toggle("is-open");const r=document.querySelector(".ex-rating-active"),s=document.querySelector(".modal-rating-value");r.style.width=await`${parseInt(s.textContent)/.05}%`}}catch(e){throw new Error(e.message)}}function p(t){const e=JSON.parse(localStorage.getItem(n)),r=JSON.stringify(e.filter(s=>s._id!==t));localStorage.setItem(n,r)}function m(t){document.querySelector(`[data-id="${t}"]`).parentElement.parentElement.parentElement}const h=document.querySelector(".favorites-contanier-block");h.addEventListener("click",t=>{if(t.target.dataset.id){const e=t.target.dataset.id;p(t.target.dataset.id),m(e)}});function y(){const t=JSON.parse(localStorage.getItem("favoriteData"))||[],e=document.querySelector(".favorites-contanier-block"),r=document.querySelector(".pagination-mobile-list");e.innerHTML="",t.length===0&&(e.innerHTML=`<img class="message-info-svg" src="./img/dumbbell.png" alt="dumbbell" />
       <p class="message-info-text">
         It appears that you haven't added any exercises to your favorites yet.
         To get started, you can add exercises that you like to your favorites
         for easier access in the future.
-      </p>`);const a=document.createElement("ul");a.classList.add("favorites-list"),e.appendChild(a);const o=t.map(s=>`
-    <li class="favorites-list-item" >
+      </p>`,r.style.display="none");const s=document.createElement("ul");s.classList.add("favorites-list"),e.appendChild(s);const a=t.map(o=>`
+
+    <li class="favorites-list-item" tabindex="0">
           <div class="favorites-card-heder">
             <div class="favorites-oval">
               <span>WORKOUT</span>
-              <button class="favorites-icon-svg" data-id="${s._id}">
+              <button class="favorites-icon-svg" data-id="${o._id}">
                 <svg width="16" height="16" fill="none">
                   <use href="./img/sprite.svg#trash"></use>
                 </svg>
@@ -30,26 +31,26 @@ import{g as f,c as u,L as n}from"./assets/add-to-favorites-84c8ee5e.js";import".
             <svg class="favorite-list-svg" width="24" height="24">
               <use href="./img/sprite.svg#dude"></use>
             </svg>
-            <h3 class="favorites-list-text">${s.name}</h3>
+            <h3 class="favorites-list-text">${o.name}</h3>
           </div>
           <div class="favorites-card-text">
             <ul class="favorites-card-text-list">
               <li class="favorites-card-text-item">
                 <div class="favorites-card-text-wrapper">
                   <h4 class="favorites-card-text-title">Burned calories:</h4>
-                  <p class="favorites-card-text-block">${s.burnedCalories}</p>
+                  <p class="favorites-card-text-block">${o.burnedCalories}</p>
                 </div>
                 <div class="favorites-card-text-wrapper">
                   <h4 class="favorites-card-text-title">Body part:</h4>
-                  <p class="favorites-card-text-block">${s.bodyPart}</p>
+                  <p class="favorites-card-text-block">${o.bodyPart}</p>
                 </div>
                 <div class="favorites-card-text-wrapper">
                   <h4 class="favorites-card-text-title">Target:</h4>
-                  <p class="favorites-card-text-block">${s.target}</p>
+                  <p class="favorites-card-text-block">${o.target}</p>
                 </div>
               </li>
             </ul>
           </div>
         </li>
-    `).join("");a.innerHTML=o}document.addEventListener("DOMContentLoaded",y);
+    `).join("");s.innerHTML=a}document.addEventListener("DOMContentLoaded",y);
 //# sourceMappingURL=commonHelpers.js.map
