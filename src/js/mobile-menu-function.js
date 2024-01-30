8c4faeb24bfd55aff6a50985d709fb69c5e6707a
andriishmorgan <andrey.shmorgan@gmail.com>
2024-01-30 10:58:56 +0100

    functionality for the mobile-menu

---
+++ src/js/mobile-menu-function.js
@@ -0,0 +1,31 @@
+/* Відкриття і закриття модального мобільного вікна */
+const burgerButton = document.querySelector('.header-burger');
+const mobileMenuElement = document.querySelector('.mobile-menu-wrapper');
+const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
+
+/* Функціонал модального мобільного вікна */
+const homeButton = document.querySelector('.mobile-menu-btn-content-home');
+const favoritesButton = document.querySelector('.mobile-menu-btn-content-favorites');
+
+/* Відкриття і закриття модального мобільного вікна */
+function openMobileMenu() {
+  mobileMenuElement.classList.remove('visually-hidden');
+}
+
+function closeMobileMenu() {
+  mobileMenuElement.classList.add('visually-hidden');
+}
+
+burgerButton.addEventListener('click', openMobileMenu);
+
+mobileMenuCloseBtn.addEventListener('click', closeMobileMenu);
+
+/* Функціонал модального мобільного вікна */
+homeButton.addEventListener('click', () => {
+  window.location.href = "./";
+});
+
+favoritesButton.addEventListener('click', () => {
+  window.location.href = "favorites-exercises.html";
+});
+
