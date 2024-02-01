8789921c074070019acf55441a1dc8b1e85d3c3d
andriishmorgan <andrey.shmorgan@gmail.com>
2024-02-01 10:13:54 +0100

    fix

--- src/js/mobile-menu-function.js
+++ src/js/mobile-menu-function.js
@@ -29,3 +29,32 @@ favoritesButton.addEventListener('click', () => {
   window.location.href = "favorites.html";
 });
 
+// /*  Отримання всіх посилань у мобільному меню */
+//   const mobileMenuLinks = document.querySelectorAll('#mobile-menu-nav a');
+// /* Функція для виділення активного елемента */
+//     function highlightCurrentPage() {
+//   const currentPage = window.location.pathname;
+// /* Ітерація по всіх посиланнях */
+//     mobileMenuLinks.forEach(function(link) {
+// /* Видалення класу active з усіх посилань */
+//       link.classList.remove('active');
+// /* Додавання класу active до посилання, яке вказує на поточну сторінку */
+//       if (link.getAttribute('href') === currentPage) {
+//         link.classList.add('active');
+//       }
+//     });
+// }
+// /* Виклик функції для встановлення активного класу на старті */
+// highlightCurrentPage();
+// /* Додавання обробника подій для слідкування за зміною хеша */
+//   window.addEventListener('popstate', highlightCurrentPage);
+
+window.addEventListener('load', () => {
+  if (window.location.pathname.endsWith('favorites.html')) {
+    favoritesButton.classList.add('active');
+    homeButton.classList.remove('active');
+  } else {
+    homeButton.classList.add('active');
+    favoritesButton.classList.remove('active');
+  }
+});
