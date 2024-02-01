7757142ad203148bd295bb55b049016d3cdee433
dmeshkovGit <140300183+dmeshkovGit@users.noreply.github.com>
2024-02-01 19:02:41 +0200

    fixed

--- src/js/favorites-list.js
+++ src/js/favorites-list.js
@@ -50,6 +50,7 @@ import {renderFavorites} from './render'
 //     params: keyGen(filterType, filterSubType, page),
 //   });
 
+<<<<<<< Updated upstream
 //   try {
 //     if (response.data.results.length === 0) {
 //       throw new Error('No results found...');
@@ -74,6 +75,50 @@ export function pagination() {
     for (let i = 1; i <= pagesQuantity; i++) {
       if (i === 1) {
         paginationElements += `<li
+=======
+const itemsPerPage = 6;
+if (window.location.pathname === '/page-2.html') {
+  favoritesList = document.querySelector('.favorites-list');
+  paginationButtons = document.querySelectorAll('.favorites-pagination-block button');
+  showPage(1);
+
+  paginationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const pageNumber = parseInt(button.textContent);
+      showPage(pageNumber);
+    });
+  });
+}
+
+function showPage(pageNumber) {
+  const startIndex = (pageNumber - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const items = favoritesList.querySelectorAll('.favorites-list-item');
+  items.forEach((item, index) => {
+    if (index >= startIndex && index < endIndex) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
+  export const cardsPerPage = 8;
+  export function pagination() {
+    const favoritesWorkout = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    let paginationElements = '';
+    // if (error) {
+    //   paginationList.innerHTML = '';
+    //   return;
+    // }
+    const pagesQuantity = Math.ceil(favoritesWorkout.length / cardsPerPage);
+    paginationList.innerHTML = '';
+    if (pagesQuantity > 1) {
+      for (let i = 1; i <= pagesQuantity; i++) {
+        if (i === 1) {
+          paginationElements += `<li
+>>>>>>> Stashed changes
                 class="exersizes-pagination-item exersizes-pagination-item-active"
               >
 
@@ -86,8 +131,8 @@ export function pagination() {
                   ${i}
                 </button>
               </li>`;
-      } else {
-        paginationElements += `<li
+        } else {
+          paginationElements += `<li
                 class="exersizes-pagination-item"
               >
 
@@ -100,27 +145,28 @@ export function pagination() {
                   ${i}
                 </button>
               </li>`;
+        }
       }
     }
-  }
-  paginationList.insertAdjacentHTML('beforeend', paginationElements);
-};
+    paginationList.insertAdjacentHTML('beforeend', paginationElements);
+  };
 
-function changingPaginationBtnStyle(e) {
-  const pageNumber = e.target.textContent.trim();
-  const previousActiveBtn = document.querySelector('.exersizes-pagination-item-active');
-  const currentActiveBtn = e.target.parentElement;
-  console.log(currentActiveBtn);
-  previousActiveBtn.classList.remove('exersizes-pagination-item-active');
-  currentActiveBtn.classList.add('exersizes-pagination-item-active');
-  currentActiveBtn.style.border = 'var(--primary-color) 1px solid';
-  currentActiveBtn.style.color = 'var(--primary-color)';
-  previousActiveBtn.style.border = 'none';
-  previousActiveBtn.style.color = 'rgba(27, 27, 27, 0.4)';
-};
+  function changingPaginationBtnStyle(e) {
+    const pageNumber = e.target.textContent.trim();
+    const previousActiveBtn = document.querySelector('.exersizes-pagination-item-active');
+    const currentActiveBtn = e.target.parentElement;
+    console.log(currentActiveBtn);
+    previousActiveBtn.classList.remove('exersizes-pagination-item-active');
+    currentActiveBtn.classList.add('exersizes-pagination-item-active');
+    currentActiveBtn.style.border = 'var(--primary-color) 1px solid';
+    currentActiveBtn.style.color = 'var(--primary-color)';
+    previousActiveBtn.style.border = 'none';
+    previousActiveBtn.style.color = 'rgba(27, 27, 27, 0.4)';
+  };
 
-const paginationList = document.querySelector('.favorites-pagination-block');
+  const paginationList = document.querySelector('.favorites-pagination-block');
 
+<<<<<<< Updated upstream
 paginationList.addEventListener("click", (e) => {
   if (e.target.classList.contains('exersizes-pagination-btn')) {
     changingPaginationBtnStyle(e);
@@ -130,4 +176,14 @@ paginationList.addEventListener("click", (e) => {
   }
   
 });
+=======
+  paginationList.addEventListener("click", (e) => {
+    if (e.target.classList.contains('exersizes-pagination-btn')) {
+      changingPaginationBtnStyle(e);
+      const favoritesContainer = document.querySelector('.favorites-contanier-block');
+      favoritesContainer.innerHTML = '';
+      renderFavorites(e);
+    }
+  });
+>>>>>>> Stashed changes
 
