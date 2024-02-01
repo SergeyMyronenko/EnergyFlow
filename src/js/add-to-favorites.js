18dfa028a00bbaaa978411a4a2774fb2c72f63e7
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2024-02-01 12:41:04 +0200

    update files

--- src/js/add-to-favorites.js
+++ src/js/add-to-favorites.js
@@ -2,15 +2,17 @@ import { getData } from './exercises-modal.js';
 
 const addToFavoritesBtn = document.querySelector('.modal-button-favorites');
 const modalExercises = document.querySelector('.modal');
-
+const body = document.querySelector('body');
 modalExercises.addEventListener('click', addToFavorites);
 
 export const LOCAL_STORAGE_KEY = 'favoriteData';
-export let inLocalStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+export let inLocalStorage;
 
 async function addToFavorites(e) {
   try {
     if (e.target.classList.contains('modal-button-favorites')) {
+      inLocalStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+
       const itemId = await getData(e.target.dataset.id);
       const data = itemId.data;
       const findCopy = inLocalStorage.some(item => item._id === data._id);
@@ -24,11 +26,12 @@ async function addToFavorites(e) {
       }
       localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(inLocalStorage));
       modalHeart.style.fill = '#EEA10C';
-      // modalHeartSvg.style.fill = '#EEA10C';
+      modalHeart.style.stroke = '#EEA10C';
 
       setTimeout(() => {
         modalExercises.classList.remove('is-open');
-      }, 1000);
+      }, 700);
+      body.classList.remove('body-modal');
     }
   } catch (error) {
     console.log(error.message);
